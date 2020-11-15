5edd0752d8dc49416530fd07c3c63f9345046054
Dobuzi <kimjw7981@gmail.com>
2020-11-15 13:34:46 +0900

    Initial Setting

---
+++ README.md
@@ -0,0 +1,3 @@
+# Snow Seeker
+
+All resources from Hacking with Swift (https://www.hackingwithswift.com)
--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    let resorts: [Resort] = Resort.allResorts
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink(
+                    destination: Text(resort.name)) {
+                    Label(
+                        title: {
+                            VStack(alignment: .leading) {
+                                Text(resort.name)
+                                    .font(.headline)
+                                Text("\(resort.runs) runs")
+                                    .foregroundColor(.secondary)
+                            }
+                        },
+                        icon: {
+                            Image(resort.country)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 25)
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(Color.black, lineWidth: 1)
+                            )
+                        }
+                    )
+                }
+            }
+            .navigationBarTitle("Resorts")
+        }
     }
 }
 
